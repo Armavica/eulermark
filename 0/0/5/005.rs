f1d65da4ef47e2aa76f303447477f0a3138cc446
Jorge Aparicio <japaric@linux.com>
2014-01-10 17:04:13 -0500

    005.rs: use println macro

--- 0/0/5/005.rs
+++ 0/0/5/005.rs
@@ -1,9 +1,7 @@
 // Copyright (C) 2014 Jorge Aparicio
 
 fn main() {
-  println(range(2, 21).
-          fold(1, |a, b| lcm(a, b)).
-          to_str());
+  println!("{}", range(2, 21).fold(1, |a, b| lcm(a, b)));
 }
 
 fn lcm(a: int, b: int) -> int {
