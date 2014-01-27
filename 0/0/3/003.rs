3f365447d2441704aed70aa19571d40b7aa39496
Jorge Aparicio <japaric@linux.com>
2014-01-26 22:34:32 -0500

    003.rs: indentation, unsigned

--- 0/0/3/003.rs
+++ 0/0/3/003.rs
@@ -1,32 +1,33 @@
 // Copyright (C) 2013 Jorge Aparicio
 
 fn main() {
-  println!("{}", factorize(600851475143).last().unwrap());
+    println!("{}", factorize(600851475143).last().unwrap());
 }
 
-struct Factorize { num: int, factor: int }
+struct Factorize { num: uint, factor: uint }
 
-fn factorize(n: int) -> Factorize {
-  Factorize { num: n, factor: 2 }
+fn factorize(n: uint) -> Factorize {
+    Factorize { num: n, factor: 2 }
 }
 
-impl Iterator<int> for Factorize {
-  fn next(&mut self) -> Option<int> {
-    if self.num == 1 {
-      None
-    } else {
-      while self.num % self.factor != 0 {
-        self.factor += 1;
+impl Iterator<uint> for Factorize {
+    fn next(&mut self) -> Option<uint> {
+        if self.num == 1 {
+            None
+        } else {
+            while self.num % self.factor != 0 {
+                if self.factor * self.factor > self.num {
+                    self.factor = self.num;
+                    break;
+                }
 
-        if self.factor * self.factor > self.num {
-          self.factor = self.num;
-          break;
+                self.factor += if self.factor % 2 == 0 { 1 } else { 2 };
+            }
+
+            self.num /= self.factor;
+
+            Some(self.factor)
         }
-      }
-
-      self.num /= self.factor;
-      Some(self.factor)
     }
-  }
 }
 
--- 0/0/3/README.md
+++ 0/0/3/README.md
@@ -7,7 +7,7 @@ What is the largest prime factor of the number 600851475143 ?
 
 Language | Time | rTime | Mem | rMem | LoC
 --- | :---: | :---: | :---: | :---: | :---:
-C | **597 us** | 100% | **1.91 MB** | 100% | 18
-haskell | 1.36 ms | 228% | 7.17 MB | 375% | 15
-rust | 1.66 ms | 278% | 6.33 MB | 331% | 24
-python | 24.9 ms | 4171% | 24.8 MB | 1298% | 12
+C | **598 us** | 100% | **1.78 MB** | 100% | 18
+haskell | 1.3 ms | 217% | 7.14 MB | 401% | 15
+rust | 1.59 ms | 266% | 6.20 MB | 348% | 24
+python | 22.6 ms | 3779% | 24.8 MB | 1393% | 12
