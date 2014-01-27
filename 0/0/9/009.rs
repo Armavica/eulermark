8bbdf901bdea3b9ce68c97382eb661d6767603b2
Jorge Aparicio <japaric@linux.com>
2014-01-27 02:14:34 -0500

    009.rs: indentation, unsigned

--- 0/0/9/009.rs
+++ 0/0/9/009.rs
@@ -1,18 +1,20 @@
 // Copyright (C) 2014 Jorge Aparicio
 
-static P: int = 1000;
+use std::num::pow;
+
+static P: uint = 1000;
 
 fn main() {
-  for c in range(P / 3 + 1, P / 2) {
-    for b in range((P - c) / 2 + 1, c) {
-      let a = P - b - c;
+    for c in range(P / 3 + 1, P / 2) {
+        for b in range((P - c) / 2 + 1, c) {
+            let a = P - b - c;
 
-      if a*a + b*b == c*c {
-        println!("{}", a * b * c);
+            if pow(a, 2) + pow(b, 2) == pow(c, 2) {
+                println!("{}", a * b * c);
 
-        return;
-      }
+                return;
+            }
+        }
     }
-  }
 }
 
