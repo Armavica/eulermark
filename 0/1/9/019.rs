c8cad7567e92fb69222d76b625ee2e8f40fe85b7
Jorge Aparicio <japaric@linux.com>
2014-01-27 13:26:41 -0500

    019.rs: indentation, unsigned, remove dead code

--- 0/1/9/019.rs
+++ 0/1/9/019.rs
@@ -1,68 +1,57 @@
 // Copyright (C) 2014 Jorge Aparicio
 
 use std::iter::range_inclusive;
-use std::num::Bounded;
 
 #[deriving(Eq,FromPrimitive)]
 enum Day {
-  Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
 }
 
 impl Day {
-  fn advance(&self, days: int) -> Day {
-    FromPrimitive::from_int((*self as int + days) % 7).unwrap()
-  }
+    fn advance(&self, days: uint) -> Day {
+        FromPrimitive::from_uint((*self as uint + days) % 7).unwrap()
+    }
 }
 
 #[deriving(FromPrimitive)]
 enum Month {
-  January, February, March, April, May, June, July, August, September, October,
-  November, December
+    January, February, March, April, May, June, July, August, September,
+    October, November, December
 }
 
 impl Month {
-  fn days(&self, year: int) -> int {
-    match *self {
-      January | March | May | July | August | October | December => 31,
-      April | June | September | November => 30,
-      _ => 28 + if year % 400 == 0 {
-                  1
+    fn days(&self, year: uint) -> uint {
+        match *self {
+            January | March | May | July | August | October | December => 31,
+            April | June | September | November => 30,
+            _ => 28 + if year % 400 == 0 {
+                    1
                 } else if year % 100 == 0 {
-                  0
+                    0
                 } else if year % 4 == 0 {
-                  1
+                    1
                 } else {
-                  0
+                    0
                 }
+        }
     }
-  }
-}
-
-impl Bounded for Month {
-  fn min_value() -> Month {
-    January
-  }
-
-  fn max_value() -> Month {
-    December
-  }
 }
 
 fn main() {
-  let mut count = 0;
-  let mut today = Monday;
+    let mut count = 0;
+    let mut today = Monday;
 
-  for year in range_inclusive(1900, 2000) {
-    for m in range_inclusive(January as int, December as int) {
-      let month: Month = FromPrimitive::from_int(m).unwrap();
+    for y in range_inclusive(1900u, 2000) {
+        for m in range_inclusive(January as uint, December as uint) {
+            let month: Month = FromPrimitive::from_uint(m).unwrap();
 
-      today = today.advance(month.days(year));
+            today = today.advance(month.days(y));
 
-      if year != 1900 && today == Sunday {
-        count += 1;
-      }
+            if y != 1900 && today == Sunday {
+                count += 1;
+            }
+        }
     }
-  }
 
-  println!("{}", count);
+    println!("{}", count);
 }
