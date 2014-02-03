d4a29241bdbaa89631f1f2a0926292c83bfb3005
Jorge Aparicio <japaric@linux.com>
2014-02-02 19:05:50 -0500

    028: solve in rust

---
+++ 0/2/8/028.rs
@@ -0,0 +1,15 @@
+// Copyright (C) 2014 Jorge Aparicio
+
+use std::iter::range_step_inclusive;
+
+fn main() {
+    println!("{}",
+        range_step_inclusive(3, 1001, 2).fold((1, 1), |(acc, corner), size| {
+            let step = size - 1;
+
+            range(0, 4).fold((acc, corner), |(a, c), _| {
+                let next_c = c + step;
+                (a + next_c, next_c)
+            })
+        }).n0());
+}
