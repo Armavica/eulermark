a4da9ba6231fd80c6b4f38054237ad64dd892b63
Jorge Aparicio <japaric@linux.com>
2014-02-17 18:19:38 -0500

    005.rs: use lcm in std::num

--- 0/0/5/005.rs
+++ 0/0/5/005.rs
@@ -1,30 +1,8 @@
 // Copyright (C) 2014 Jorge Aparicio
 
-use std::util::{replace,swap};
+use std::num::lcm;
 
 fn main() {
     println!("{}", range(2, 21u).fold(1, |a, b| lcm(a, b)));
-}
-
-fn lcm(a: uint, b: uint) -> uint {
-    a * b / gcd(a, b)
-}
-
-fn gcd(mut a: uint, mut b: uint) -> uint {
-    if a < b {
-        swap(&mut a, &mut b);
-    }
-
-    loop {
-        let r = a % b;
-
-        if r == 0 {
-            break;
-        }
-
-        a = replace(&mut b, r);
-    }
-
-    b
 }
 
