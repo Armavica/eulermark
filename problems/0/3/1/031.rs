d9efc75552458b148eba3c7ffc27bc2365fc5051
Jorge Aparicio <japaric@linux.com>
2014-03-19 10:08:31 -0500

    031: solve in rust

---
+++ problems/0/3/1/031.rs
@@ -0,0 +1,95 @@
+// Copyright (C) 2014 Jorge Aparicio
+
+extern crate collections;
+
+use collections::HashSet;
+use std::vec_ng::Vec;
+
+static NPENCES: u8 = 8;
+
+enum Pence {
+    One,
+    Two,
+    Five,
+    Ten,
+    Twenty,
+    Fifty,
+    OneHundred,
+    TwoHundred,
+}
+
+static PENCES: [Pence, ..NPENCES] = [One, Two, Five, Ten, Twenty, Fifty,
+    OneHundred, TwoHundred];
+
+impl Pence {
+    fn value(&self) -> u8 {
+        match *self {
+            One => 1,
+            Two => 2,
+            Five => 5,
+            Ten => 10,
+            Twenty => 20,
+            Fifty => 50,
+            OneHundred => 100,
+            TwoHundred => 200,
+        }
+    }
+}
+
+#[deriving(Clone,Eq,Hash)]
+struct Change {
+    pences: Vec<u8>,
+}
+
+impl Change {
+    fn new(amount: u8) -> Change {
+        let mut pences = Vec::from_elem(NPENCES as uint, 0u8);
+        *pences.get_mut(One as uint) = amount;
+
+        Change { pences: pences }
+    }
+
+    fn expand(&self) -> Vec<Change> {
+        let mut out = Vec::new();
+
+        for &pence in PENCES.iter().skip(1) {
+            let v = pence.value();
+
+            if *self.pences.get(One as uint) >= v {
+                let mut tmp = self.clone();
+
+                *tmp.pences.get_mut(One as uint) -= v;
+                *tmp.pences.get_mut(pence as uint) += 1;
+
+                out.push(tmp);
+            }
+        }
+
+        out
+    }
+}
+
+fn main() {
+    let mut row = HashSet::new();
+    let mut count = 0;
+
+    row.insert(Change::new(200));
+
+    while row.len() > 0 {
+        let mut tmp = HashSet::new();
+        count += row.len();
+
+        for parent in row.iter() {
+            let children = parent.expand();
+
+            for child in children.move_iter() {
+                tmp.insert(child);
+            }
+        }
+
+        row = tmp;
+    }
+
+    println!("{}", count);
+}
+
