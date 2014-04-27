7a5025fa5e55d5571af73a52b230d585f0938d6a
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-04-27 21:15:30 +0200

    Speedup of ~60000 of 025.rs

--- problems/0/2/5/025.rs
+++ problems/0/2/5/025.rs
@@ -3,30 +3,22 @@
 extern crate num;
 
 use num::bigint::BigUint;
-use num::bigint::ToBigUint;
 use std::mem::replace;
-use std::num::One;
-use std::num::Zero;
+use std::num::{One, Zero};
 
 fn main() {
-    println!("{}", fibonacci().enumerate().skip_while(|&(_, ref n)| number_of_digits(n.clone()) < 1000).next().unwrap().val0() + 1);
-}
-
-fn number_of_digits(mut n: BigUint) -> uint {
-    let mut nod = 0;
-
-    while !n.is_zero() {
-        nod += 1;
-        n = n / 10u.to_biguint().unwrap();
-    }
-
-    nod
+    let thousand_digits = BigUint::parse_bytes(Vec::from_fn(1000, |i| if i==0 {1} else {0} + 48).as_slice(), 10).unwrap();
+    println!("{}",
+              fibonacci()
+             .enumerate()
+             .skip_while(|&(_, ref n)| n < &thousand_digits)
+             .next().unwrap().val0());
 }
 
 struct Fibonacci { curr: BigUint, next: BigUint }
 
 fn fibonacci() -> Fibonacci {
-    Fibonacci { curr: One::one(), next: One::one() }
+    Fibonacci { curr: Zero::zero(), next: One::one() }
 }
 
 impl Iterator<BigUint> for Fibonacci {
